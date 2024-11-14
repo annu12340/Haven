16c18c4a72ef1a12a28b93a8f72de06f43af441c
Kavikumar M <kavikumarceo@gmail.com>
2024-11-14 06:04:53 +0530

    added readme

--- backend/main.py
+++ backend/main.py
@@ -39,7 +39,7 @@ db = None
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://amazonbedrock.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
